2472a0f0b90ee444ecab937ea870f524402551b3
MohammedNishat <149462755+MohammedNishat@users.noreply.github.com>
2023-11-27 16:55:34 +0530

    Update Demo1.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,6 @@
 
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("Hello World from jenkins");
+	System.out.println("Hello World from jenkins from Webhooks");
 }
 }
